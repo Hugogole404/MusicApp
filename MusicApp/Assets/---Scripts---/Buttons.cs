b74cde65cfc560c90bfbec69f9e85926bb926f35
P3D\hugo.lestriez <lestriehug@gmail.com>
2024-09-24 09:09:02 +0200

    Init Commit

---
+++ MusicApp/Assets/---Scripts---/Buttons.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] private float _sizeMultiplicator;
+    [SerializeField] private float _durationTime;
+    public void OnClickPlaylist()
+    {
+
+    }
+    public void OnClickPlay()
+    {
+
+    }    
+    public void OnClickOptions()
+    {
+
+    }    
+    public void OnClickExit()
+    {
+
+    }
+}
